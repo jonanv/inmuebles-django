0e58e954c2bb631ab114a38189b878acbda328e9
jonanv <jonan-vargas23@hotmail.com>
2025-03-08 14:40:22 -0500

    :sparkles: (inmueblesapp): Create inmuebles model

--- inmuebles/inmueblesapp/models.py
+++ inmuebles/inmueblesapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Inmueble model
+class Inmueble(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    pais = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='inmuebles', null=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
