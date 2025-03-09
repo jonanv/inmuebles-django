33d09324a4899330c138016e84d02d6f868d3f8c
jonanv <jonan-vargas23@hotmail.com>
2025-03-09 15:46:00 -0500

    :sparkles: (inmueblesapp): Create nure route for get inmueble by id

--- inmuebles/inmueblesapp/views.py
+++ inmuebles/inmueblesapp/views.py
@@ -10,3 +10,17 @@ def list_all_inmuebles(request) -> JsonResponse:
     }
 
     return JsonResponse(data, safe=False)
+
+def get_inmueble_by_id(request, id) -> JsonResponse:
+    inmueble = Inmueble.objects.get(id=id)
+    data = {
+        'nombre': inmueble.nombre,
+        'direccion': inmueble.direccion,
+        'pais': inmueble.pais,
+        'ciudad': inmueble.ciudad,
+        'descripcion': inmueble.descripcion,
+        'imagen': inmueble.imagen,
+        'active': inmueble.active,
+    }
+
+    return JsonResponse(data, safe=False)
