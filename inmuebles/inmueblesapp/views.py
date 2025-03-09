620eb94dfc877e4527af61ce72fea9b7610afd6e
jonanv <jonan-vargas23@hotmail.com>
2025-03-09 16:44:32 -0500

    :fire: (inmueblesapp): Remove methods od views

--- inmuebles/inmueblesapp/views.py
+++ inmuebles/inmueblesapp/views.py
@@ -1,26 +1,26 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
-from .models import Inmueble
+# from .models import Inmueble
 
-# Create your views here.
-def list_all_inmuebles(request) -> JsonResponse:
-    inmuebles = Inmueble.objects.all()
-    data = {
-        'inmuebles': list(inmuebles.values())
-    }
+# # Create your views here.
+# def list_all_inmuebles(request) -> JsonResponse:
+#     inmuebles = Inmueble.objects.all()
+#     data = {
+#         'inmuebles': list(inmuebles.values())
+#     }
 
-    return JsonResponse(data, safe=False)
+#     return JsonResponse(data, safe=False)
 
-def get_inmueble_by_id(request, id) -> JsonResponse:
-    inmueble = Inmueble.objects.get(id=id)
-    data = {
-        'nombre': inmueble.nombre,
-        'direccion': inmueble.direccion,
-        'pais': inmueble.pais,
-        'ciudad': inmueble.ciudad,
-        'descripcion': inmueble.descripcion,
-        'imagen': inmueble.imagen,
-        'active': inmueble.active,
-    }
+# def get_inmueble_by_id(request, id) -> JsonResponse:
+#     inmueble = Inmueble.objects.get(id=id)
+#     data = {
+#         'nombre': inmueble.nombre,
+#         'direccion': inmueble.direccion,
+#         'pais': inmueble.pais,
+#         'ciudad': inmueble.ciudad,
+#         'descripcion': inmueble.descripcion,
+#         'imagen': inmueble.imagen,
+#         'active': inmueble.active,
+#     }
 
-    return JsonResponse(data, safe=False)
+#     return JsonResponse(data, safe=False)
