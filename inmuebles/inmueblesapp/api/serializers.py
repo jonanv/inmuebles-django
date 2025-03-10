536e42697fc5ea4858aa6cf4bd602d6c0b0e24b9
jonanv <jonan-vargas23@hotmail.com>
2025-03-10 16:41:00 -0500

    :sparkles: (inmueblesapp): Implement core arguments into serialization model

--- inmuebles/inmueblesapp/api/serializers.py
+++ inmuebles/inmueblesapp/api/serializers.py
@@ -3,38 +3,11 @@ from rest_framework import serializers
 # Imports
 from ..models import Inmueble
 
-def column_long_validator(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('El valor es demasiado corto')
+class InmuebleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inmueble
+        fields = '__all__'
 
-class InmuebleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    nombre = serializers.CharField()
-    direccion = serializers.CharField(validators=[column_long_validator])
-    pais = serializers.CharField(validators=[column_long_validator])
-    ciudad = serializers.CharField()
-    descripcion = serializers.CharField()
-    imagen = serializers.CharField()
-    active = serializers.BooleanField(default=True)
-
-    def create(self, validated_data):
-        return Inmueble.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.nombre = validated_data.get('nombre', instance.nombre)
-        instance.direccion = validated_data.get('direccion', instance.direccion)
-        instance.pais = validated_data.get('pais', instance.pais)
-        instance.ciudad = validated_data.get('ciudad', instance.ciudad)
-        instance.descripcion = validated_data.get('descripcion', instance.descripcion)
-        instance.imagen = validated_data.get('imagen', instance.imagen)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    
-    def delete(self, instance):
-        instance.delete()
-        return instance
-    
     def validate(self, data):
         if data['direccion'] == data['pais']:
             raise serializers.ValidationError('La dirección no puede ser igual al país')
@@ -44,3 +17,50 @@ class InmuebleSerializer(serializers.Serializer):
         if len(data) < 2:
             raise serializers.ValidationError('La url de la imagen es demasiado corta')
         return data
+
+
+
+
+
+
+# def column_long_validator(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError('El valor es demasiado corto')
+
+# class InmuebleSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     nombre = serializers.CharField()
+#     direccion = serializers.CharField(validators=[column_long_validator])
+#     pais = serializers.CharField(validators=[column_long_validator])
+#     ciudad = serializers.CharField()
+#     descripcion = serializers.CharField()
+#     imagen = serializers.CharField()
+#     active = serializers.BooleanField(default=True)
+
+#     def create(self, validated_data):
+#         return Inmueble.objects.create(**validated_data)
+    
+#     def update(self, instance, validated_data):
+#         instance.nombre = validated_data.get('nombre', instance.nombre)
+#         instance.direccion = validated_data.get('direccion', instance.direccion)
+#         instance.pais = validated_data.get('pais', instance.pais)
+#         instance.ciudad = validated_data.get('ciudad', instance.ciudad)
+#         instance.descripcion = validated_data.get('descripcion', instance.descripcion)
+#         instance.imagen = validated_data.get('imagen', instance.imagen)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+#         return instance
+    
+#     def delete(self, instance):
+#         instance.delete()
+#         return instance
+    
+#     def validate(self, data):
+#         if data['direccion'] == data['pais']:
+#             raise serializers.ValidationError('La dirección no puede ser igual al país')
+#         return data
+        
+#     def validate_imagen(self, data):
+#         if len(data) < 2:
+#             raise serializers.ValidationError('La url de la imagen es demasiado corta')
+#         return data
