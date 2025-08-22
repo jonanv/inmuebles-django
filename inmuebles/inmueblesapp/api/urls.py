621a71e80fa7cf458849db2b65d2b654127fdb2a
jonanv <jonan-vargas23@hotmail.com>
2025-08-21 20:14:32 -0500

    :truck: (inmueblesapp): Rename urls old without rest_framework

--- inmuebles/inmueblesapp/api/urls.py
+++ inmuebles/inmueblesapp/api/urls.py
@@ -4,11 +4,6 @@ from django.urls import path
 from . import views
 
 app_name = 'inmueblesapp'
-# urlpatterns = [
-#     path('list/', views.list_all_inmuebles, name='list-all-inmuebles'),
-#     path('<int:id>/', views.get_inmueble_by_id, name='get-inmueble-by-id'),
-# ]
-
 urlpatterns = [
     path('list/', views.InmueblesListAV.as_view(), name='list-all-inmuebles'),
     path('<int:id>/', views.InmuebleDetailAV.as_view(), name='get-inmueble-by-id'),
