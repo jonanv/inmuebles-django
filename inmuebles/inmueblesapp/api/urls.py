c4db87719ac51c632bb0dd46faae8d449ff65db3
jonanv <jonan-vargas23@hotmail.com>
2025-11-22 18:16:25 -0500

    ✨ Add url for path empresa

--- inmuebles/inmueblesapp/api/urls.py
+++ inmuebles/inmueblesapp/api/urls.py
@@ -7,4 +7,5 @@ app_name = 'inmueblesapp'
 urlpatterns = [
     path('list/', views.InmueblesListAV.as_view(), name='list-all-inmuebles'),          # /inmueble/list/
     path('<int:id>/', views.InmuebleDetailAV.as_view(), name='get-inmueble-by-id'),     # /inmueble/1/
+    path('empresa/list/', views.EmpresaListAV.as_view(), name='list-all-empresas'),     # /inmueble/empresa/list/
 ]
