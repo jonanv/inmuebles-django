365479c27c4562ac9f44b90801b9a936f154a1c0
jonanv <jonan-vargas23@hotmail.com>
2025-11-24 12:11:04 -0500

    ✨ Register empresa into admin.py

--- inmuebles/inmueblesapp/admin.py
+++ inmuebles/inmueblesapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Inmueble
+from .models import Empresa, Inmueble
 
+# Register your models here.
 admin.site.register(Inmueble)
+admin.site.register(Empresa)
