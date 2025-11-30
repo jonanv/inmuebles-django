3df1d4155e4845c80349afdf4215c054e24d1fc2
jonanv <jonan-vargas23@hotmail.com>
2025-11-30 14:29:48 -0500

    🎨 Create field foreingkey empresa into model Edificacion

--- inmuebles/inmueblesapp/models.py
+++ inmuebles/inmueblesapp/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 
 # Create your models here.
-# Edificacion model
-class Edificacion(models.Model):
-    direccion = models.CharField(max_length=200)
-    pais = models.CharField(max_length=100)
-    ciudad = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    imagen = models.CharField(max_length=300)
-    active = models.BooleanField(default=True)
-    create = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return self.direccion
-    
 # Empresa model
 class Empresa(models.Model):
     nombre = models.CharField(max_length=250)
@@ -25,3 +12,18 @@ class Empresa(models.Model):
 
     def __str__(self) -> str:
         return self.nombre
+
+# Edificacion model
+class Edificacion(models.Model):
+    direccion = models.CharField(max_length=200)
+    pais = models.CharField(max_length=100)
+    ciudad = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    imagen = models.CharField(max_length=300)
+    active = models.BooleanField(default=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, related_name="edificacion")
+    create = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.direccion
