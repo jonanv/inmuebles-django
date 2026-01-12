360d1454d9b41a8b8b18277b3a508512e075d5a2
jonanv <jonan-vargas23@hotmail.com>
2026-01-12 14:14:29 -0500

    ✨ Add new route to URL for empresa detail

--- inmuebles/inmueblesapp/api/urls.py
+++ inmuebles/inmueblesapp/api/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'inmueblesapp'
 urlpatterns = [
     path('list/', views.EdificacionesListAV.as_view(), name='list-all-inmuebles'),          # /inmueble/list/
-    path('<int:pk>/', views.EdificacionDetailAV.as_view(), name='get-inmueble-by-id'),     # /inmueble/1/
-    path('empresa/list/', views.EmpresaListAV.as_view(), name='list-all-empresas'),     # /inmueble/empresa/list/
+    path('<int:pk>/', views.EdificacionDetailAV.as_view(), name='get-inmueble-by-id'),      # /inmueble/1/
+    path('empresa/list/', views.EmpresaListAV.as_view(), name='list-all-empresas'),         # /inmueble/empresa/list/
+    path('empresa/<int:pk>/', views.EmpresaDetailAV.as_view(), name='get-empresa-by-id'),   # /inmueble/empresa/1/
 ]
