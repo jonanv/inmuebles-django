ad9fcd928a813a20eb55710108a2814b66f17e16
jonanv <jonan-vargas23@hotmail.com>
2026-01-16 15:14:39 -0500

    ✨ Create model comentario

--- inmuebles/inmueblesapp/models.py
+++ inmuebles/inmueblesapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -27,3 +28,15 @@ class Edificacion(models.Model):
 
     def __str__(self) -> str:
         return self.direccion
+    
+# Comentario model
+class Comentario(models.Model):
+    calificacion = models.PositiveBigIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    texto = models.CharField(max_length=200, null=True)
+    edificacion = models.ForeignKey(Edificacion, on_delete=models.CASCADE, related_name="comentarios")
+    active = models.BooleanField(default=True)
+    create = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.calificacion)
