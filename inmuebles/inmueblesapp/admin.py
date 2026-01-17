f9a4f54d453b535651500484f332f6c3e8f88a5b
jonanv <jonan-vargas23@hotmail.com>
2026-01-17 18:24:01 -0500

    ✨ Register model Comentario into admin

--- inmuebles/inmueblesapp/admin.py
+++ inmuebles/inmueblesapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Empresa, Edificacion
+from .models import Empresa, Edificacion, Comentario
 
 # Register your models here.
 admin.site.register(Edificacion)
 admin.site.register(Empresa)
+admin.site.register(Comentario)
