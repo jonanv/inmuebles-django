461641cfd0aded925c575449cd8d3528f6e6d234
jonanv <jonan-vargas23@hotmail.com>
2026-01-24 14:19:30 -0500

    ✨ Add new url for ComentarioDetailGAV

--- inmuebles/inmueblesapp/api/urls.py
+++ inmuebles/inmueblesapp/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('empresa/list/', views.EmpresaListAV.as_view(), name='list-all-empresas'),                 # /inmueble/empresa/list/
     path('empresa/<int:pk>/', views.EmpresaDetailAV.as_view(), name='get-empresa-by-id'),           # /inmueble/empresa/1/
     path('comentario/list/', views.ComentarioListGAV.as_view(), name='list-all-comentarios'),       # /inmueble/comentario/list/
+    path('comentario/<int:pk>/', views.ComentarioDetailGAV.as_view(), name='get-comentario-by-id'), # /inmueble/comentario/list/
 ]
