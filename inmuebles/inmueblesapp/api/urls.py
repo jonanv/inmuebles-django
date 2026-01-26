3a2dd94923023bdbaa6e1812e154bd00484bd470
jonanv <jonan-vargas23@hotmail.com>
2026-01-26 13:32:06 -0500

    🎨 Change URL inmueble to tienda and URL app inmuebles

--- inmuebles/inmueblesapp/api/urls.py
+++ inmuebles/inmueblesapp/api/urls.py
@@ -5,10 +5,12 @@ from . import views
 
 app_name = 'inmueblesapp'
 urlpatterns = [
-    path('list/', views.EdificacionesListAV.as_view(), name='list-all-inmuebles'),                  # /inmueble/list/
-    path('<int:pk>/', views.EdificacionDetailAV.as_view(), name='get-inmueble-by-id'),              # /inmueble/1/
-    path('empresa/list/', views.EmpresaListAV.as_view(), name='list-all-empresas'),                 # /inmueble/empresa/list/
+    path('edificacion/', views.EdificacionesListAV.as_view(), name='list-all-inmuebles'),                  # /inmueble/list/
+    path('edificacion/<int:pk>/', views.EdificacionDetailAV.as_view(), name='get-inmueble-by-id'),              # /inmueble/1/
+    
+    path('empresa/', views.EmpresaListAV.as_view(), name='list-all-empresas'),                 # /inmueble/empresa/list/
     path('empresa/<int:pk>/', views.EmpresaDetailAV.as_view(), name='get-empresa-by-id'),           # /inmueble/empresa/1/
-    path('comentario/list/', views.ComentarioListGAV.as_view(), name='list-all-comentarios'),       # /inmueble/comentario/list/
-    path('comentario/<int:pk>/', views.ComentarioDetailGAV.as_view(), name='get-comentario-by-id'), # /inmueble/comentario/list/
+    
+    path('edificacion/<int:pk>/comentario/', views.EdificacionDetailAV.as_view(), name='list-all-comentarios'),       # /inmueble/comentario/list/
+    path('edificacion/comentario/<int:pk>/', views.ComentarioDetailGAV.as_view(), name='get-comentario-by-id'), # /inmueble/comentario/list/
 ]
