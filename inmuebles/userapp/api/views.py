2e281ddb95196abcde5bf75869416cc694aea4c6
jonanv <jonan-vargas23@hotmail.com>
2026-02-24 14:37:51 -0500

    🎨 Implement method for generate auth token when a user is created

--- inmuebles/userapp/api/views.py
+++ inmuebles/userapp/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 # Imports
 from .serializers import RegistrarSerializer
@@ -9,7 +10,15 @@ from .serializers import RegistrarSerializer
 def registrar_view(request):
     if request.method == 'POST':
         serializer = RegistrarSerializer(data=request.data)
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            account = serializer.save()
+            data['response'] = 'Usuario registrado exitosamente.'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            
+            return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
