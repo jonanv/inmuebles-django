bffbcc3fa3d39deddc279505ced5ca145de8f368
jonanv <jonan-vargas23@hotmail.com>
2026-02-24 14:46:54 -0500

    🎨 Add path into urls for logout

--- inmuebles/userapp/api/urls.py
+++ inmuebles/userapp/api/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),  # Endpoint para obtener el token de autenticación
     path('registrar/', views.registrar_view, name='registrar'),  # Endpoint para registrar un nuevo usuario
+    path('logout/', views.logout_view, name='logout'),  # Endpoint para cerrar sesión (eliminar el token)
 ]
