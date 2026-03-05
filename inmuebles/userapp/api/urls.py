fc6dbefcb795f7aa1880f3e6bf9909c68ec0c8ea
jonanv <jonan-vargas23@hotmail.com>
2026-03-04 19:21:37 -0500

    ✨ Add new urls for get tokena and refresh token

--- inmuebles/userapp/api/urls.py
+++ inmuebles/userapp/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # Imports
 from . import views
@@ -8,4 +9,7 @@ urlpatterns = [
     path('login/', obtain_auth_token, name='login'),  # Endpoint para obtener el token de autenticación
     path('registrar/', views.registrar_view, name='registrar'),  # Endpoint para registrar un nuevo usuario
     path('logout/', views.logout_view, name='logout'),  # Endpoint para cerrar sesión (eliminar el token)
+    
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Endpoint para obtener el token JWT
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # Endpoint para refrescar el token JWT
 ]
