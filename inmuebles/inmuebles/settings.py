a96a831e5d895a8868673f27170cc94aff4baeed
jonanv <jonan-vargas23@hotmail.com>
2026-03-21 17:30:27 -0500

    🔧 Config time about JWT

--- inmuebles/inmuebles/settings.py
+++ inmuebles/inmuebles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,4 +153,6 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,  # Permite rotar los tokens de refresco para mayor seguridad
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # Tiempo de vida del token de acceso
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),  # Tiempo de vida del token de refresco
 }
