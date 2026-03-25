7b3bc5c5a9dba42227519d2366447ac53372b23b
jonanv <jonan-vargas23@hotmail.com>
2026-03-25 15:20:10 -0500

    🎨 Implement pagination for EdificacionList

--- inmuebles/inmueblesapp/api/views.py
+++ inmuebles/inmueblesapp/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle, ScopedRateThrottle
+from inmuebles.inmueblesapp.api.pagination import EdificacionPagination
 from inmueblesapp.api.throttling import ComentarioCreateThrottle, ComentarioListThrottle
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
@@ -70,6 +71,7 @@ class EdificacionList(generics.ListAPIView):
     serializer_class = EdificacionSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter] # Permite buscar edificaciones por dirección utilizando el parámetro de consulta ?search=calle, donde calle es parte de la dirección de la edificacion
     search_fields = ['direccion', 'empresa__nombre']
+    pagination_class = [EdificacionPagination] # Permite paginar los resultados de la lista de edificaciones utilizando la clase de paginación personalizada EdificacionPagination definida en pagination.py
 
 # Vistas para Empresa utilizando ViewSet, se puede usar con routers para generar automáticamente las rutas, pero no es necesario definir los métodos HTTP, se pueden definir métodos personalizados, reemplaza a EmpresaListAV y EmpresaDetailAV, se comenta estas dos vistas para evitar conflictos con las rutas generadas por el router, si se quieren usar ambas formas de vista, se deben definir rutas diferentes para cada una en urls.py
 class EmpresaListVS(viewsets.ModelViewSet):
