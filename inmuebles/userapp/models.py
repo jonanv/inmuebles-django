06ee15af564157bc17364e7f347c97d9b1924984
jonanv <jonan-vargas23@hotmail.com>
2026-04-07 18:45:14 -0500

    🐛 Fix error into create superuser with Account

--- inmuebles/userapp/models.py
+++ inmuebles/userapp/models.py
@@ -31,7 +31,7 @@ class MyAccountManager(BaseUserManager):
         return user
     
     def create_superuser(self, first_name, last_name, username, email, password):
-        user = self.create_user(
+        user = self.model(
             email = self.normalize_email(email),
             username = username,
             password = password,
