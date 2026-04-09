35c57f5ac281285b2251108bbe9bd39d4d28dbe8
jonanv <jonan-vargas23@hotmail.com>
2026-04-09 15:39:17 -0500

    🔧 Add conection for database postgreSQL

--- inmuebles/inmuebles/settings.py
+++ inmuebles/inmuebles/settings.py
@@ -80,10 +80,21 @@ AUTH_USER_MODEL = 'userapp.Account' # Especifica el modelo de usuario personaliz
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inmueblesdb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
