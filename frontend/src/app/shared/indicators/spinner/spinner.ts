cf139f168361b5fec9656d94a5a536a3185330f1
jonanv <jonan-vargas23@hotmail.com>
2026-04-20 18:29:07 -0500

    ✨ Create component no-standalone spinner

---
+++ frontend/src/app/shared/indicators/spinner/spinner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  templateUrl: './spinner.html',
+  styleUrl: './spinner.css',
+  standalone: false,
+})
+export class Spinner {}
