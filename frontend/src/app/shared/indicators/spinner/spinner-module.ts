40681663cf64dbf85b4cde25d1bddfcf8d0e8701
jonanv <jonan-vargas23@hotmail.com>
2026-04-21 15:08:45 -0500

    🎨 Import component spinner into spinner-module

--- frontend/src/app/shared/indicators/spinner/spinner-module.ts
+++ frontend/src/app/shared/indicators/spinner/spinner-module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Components
+import { Spinner } from './spinner';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    Spinner,
+  ],
   imports: [CommonModule],
 })
 export class SpinnerModule {}
