d8614e94e1991e09552a0b4465c058a999938171
jonanv <jonan-vargas23@hotmail.com>
2026-04-27 15:19:06 -0500

    🎨 Format component spinner

--- frontend/src/app/shared/indicators/spinner/spinner.ts
+++ frontend/src/app/shared/indicators/spinner/spinner.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
+  standalone: false,
   templateUrl: './spinner.html',
   styleUrl: './spinner.css',
-  standalone: false,
 })
 export class Spinner {}
