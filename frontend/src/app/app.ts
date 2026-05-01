28551334f8e4c7c2ad54fdb80119a865353aa7b3
jonanv <jonan-vargas23@hotmail.com>
2026-05-01 17:51:03 -0500

    🎨 Implements spinner component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Imports
+import { IndicatorsModule } from './shared/indicators';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    IndicatorsModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  public showSpinner: boolean = false;
+
+  public onToggleSpinner(): void {
+    this.showSpinner = !this.showSpinner;
+  }
 }
