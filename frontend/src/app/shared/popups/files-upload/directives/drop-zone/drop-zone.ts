c4966100be44b4bc3e19e74ed0cc0632f3046cf7
jonanv <jonan-vargas23@hotmail.com>
2026-05-15 15:46:53 -0500

    ✨ Add @HostListeners to directive drop-zone

--- frontend/src/app/shared/popups/files-upload/directives/drop-zone/drop-zone.ts
+++ frontend/src/app/shared/popups/files-upload/directives/drop-zone/drop-zone.ts
@@ -1,9 +1,34 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
   selector: '[appDropZone]',
   standalone: false,
 })
 export class DropZone {
+  @Output() dropped: EventEmitter<FileList[]> = new EventEmitter<FileList[]>();
+  @Output() hovered: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   constructor() {}
+
+  // Drag and Drop Event Listeners
+  // Dejar caer
+  @HostListener('drop', ['$event']) onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.hovered.emit(false);
+    if (event.dataTransfer?.files) {
+      this.dropped.emit([event.dataTransfer.files]);
+    }
+  }
+
+  // Arrastrar sobre
+  @HostListener('dragover', ['$event']) onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.hovered.emit(true);
+  }
+
+  // Salir del área de arrastre
+  @HostListener('dragleave', ['$event']) onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.hovered.emit(false);
+  }
 }
