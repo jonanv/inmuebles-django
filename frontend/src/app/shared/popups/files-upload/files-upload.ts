f3664b36cece3cd070415eca202520413feb68ae
jonanv <jonan-vargas23@hotmail.com>
2026-05-16 15:22:56 -0500

    🎨 Implement methods of drag and drop with directive

--- frontend/src/app/shared/popups/files-upload/files-upload.ts
+++ frontend/src/app/shared/popups/files-upload/files-upload.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef , MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  multiple: boolean;
+  crop: boolean;
+}
 
 @Component({
   selector: 'app-files-upload',
@@ -6,10 +12,48 @@ import { Component } from '@angular/core';
   templateUrl: './files-upload.html',
   styleUrl: './files-upload.scss',
 })
-export class FilesUpload {
+export class FilesUpload implements OnInit {
   public isHovering: boolean = false;
+  private files!: File[];
+  private imageFile!: File;
+  public isError!: boolean;
+  private filesURLs: string[] = [];
 
-  public toggleHovered($event: boolean): void {
+  constructor(
+    private dialogRef: MatDialogRef<FilesUpload>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) { }
 
+  public ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.files = [];
+  }
+
+  public toggleHovered(event: boolean): void {
+    this.isHovering = event;
+  }
+
+  public onDrop(files: FileList): void {
+    this.dropGeneral(files);
+  }
+
+  public onDropFile(event: FileList | any): void {
+    this.dropGeneral(event.target.files)
+  }
+
+  public dropGeneral(files: FileList): void {
+    this.isError = false;
+
+    if (this.data.crop && files.length > 1) {
+      this.isError = true;
+      return;
+    }
+
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i) as File);
+    }
+
+    console.log(files);
   }
 }
