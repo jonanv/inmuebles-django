310b1ba6af820040d49d1fe3a89e2c31a5d29086
jonanv <jonan-vargas23@hotmail.com>
2026-05-17 16:05:44 -0500

    🔧 Add config to environmnents

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,1 +1,10 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    apiKey: 'TU_API_KEY',
+    authDomain: 'TU_PROYECTO.firebaseapp.com',
+    projectId: 'TU_PROJECT_ID',
+    storageBucket: 'TU_BUCKET',
+    messagingSenderId: 'TU_SENDER_ID',
+    appId: 'TU_APP_ID'
+  }
+};
