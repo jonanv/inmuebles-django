1f973baf82e46c45d984c58efa7b58d759953df5
jonanv <jonan-vargas23@hotmail.com>
2026-05-19 13:52:21 -0500

    🙈 Add environments files to .gitignore

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  name: 'production',
+  // Your web app's Firebase configuration
+  firebase: {
+    apiKey: 'TU_API_KEY',
+    authDomain: 'TU_PROYECTO.firebaseapp.com',
+    projectId: 'TU_PROJECT_ID',
+    storageBucket: 'TU_BUCKET',
+    messagingSenderId: 'TU_SENDER_ID',
+    appId: 'TU_APP_ID'
+  }
+};
