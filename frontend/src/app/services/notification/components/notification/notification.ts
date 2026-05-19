76573c897e2359348d9669ba44cbf9e37731726d
jonanv <jonan-vargas23@hotmail.com>
2026-05-19 17:32:06 -0500

    ✨ Create component notification into folder services
    ng g component services/notification/components/notification --no-standalone

---
+++ frontend/src/app/services/notification/components/notification/notification.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  standalone: false,
+  templateUrl: './notification.html',
+  styleUrl: './notification.scss',
+})
+export class Notification {}
