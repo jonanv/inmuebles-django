5ac33ea439d7f77fb975ee6d13bc3e68b8e7d31d
jonanv <jonan-vargas23@hotmail.com>
2026-05-20 13:53:22 -0500

    🎨 Join again app-module and app with standalone for angular 21

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+// Imports
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+// Modules
+import { NotificationModule } from './services';
+import { IndicatorsModule } from './shared/indicators';
+import { PopupsModule } from './shared/popups';
 
 // Services
 import { Notification as NotificationService } from './services';
 
+// Components
+import { Header } from './components';
+
 @Component({
   selector: 'app-root',
-  standalone: false,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NotificationModule,
+    IndicatorsModule,
+    PopupsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
