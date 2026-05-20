9c732be3d0c73e61ab732ca392e6b592468ab026
jonanv <jonan-vargas23@hotmail.com>
2026-05-20 14:47:06 -0500

    ✨ Create component standalone menu-list
    ng g component components/menu-list

---
+++ frontend/src/app/components/menu-list/menu-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-list',
+  imports: [],
+  templateUrl: './menu-list.html',
+  styleUrl: './menu-list.scss',
+})
+export class MenuList {}
