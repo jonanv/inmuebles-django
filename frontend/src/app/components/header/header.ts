cefa021c73dff578d89ed32ce705485a1eb265d7
jonanv <jonan-vargas23@hotmail.com>
2026-05-20 13:54:09 -0500

    🎨 Import module to component header standalone

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  standalone: false,
+  imports: [
+    CommonModule,
+
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  standalone: true,
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
