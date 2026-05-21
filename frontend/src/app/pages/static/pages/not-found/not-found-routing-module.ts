9b75b2ab2149af74f02d149fadb743db42f22517
jonanv <jonan-vargas23@hotmail.com>
2026-05-21 15:08:57 -0500

    🎨 Add routes for components pages welcome and not-found

--- frontend/src/app/pages/static/pages/not-found/not-found-routing-module.ts
+++ frontend/src/app/pages/static/pages/not-found/not-found-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { NotFound } from './not-found';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: NotFound
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/pages/static/pages/welcome/welcome-routing-module.ts
+++ frontend/src/app/pages/static/pages/welcome/welcome-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { Welcome } from './welcome';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Welcome
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
