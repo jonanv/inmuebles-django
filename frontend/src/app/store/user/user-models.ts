a9db8dd2d814102c5979f3d7cc44284f1a207256
jonanv <jonan-vargas23@hotmail.com>
2026-05-21 18:28:37 -0500

    🐛 Fix error with interfaces user-models

--- frontend/src/app/store/user/user-models.ts
+++ frontend/src/app/store/user/user-models.ts
@@ -1,14 +1,16 @@
-import { User as UserResponse } from '../../models/backend/user/index';
+import { User } from '../../models/backend';
+
+export type { User as UserResponse } from '../../models/backend/user/index';
 
 export interface EmailPasswordCredentials {
   email: string;
   password: string;
 }
 
-export interface UserRegister extends UserResponse {
+export interface UserRegister extends User {
   password: string;
   password_confirm: string;
 }
 
 // Omite el atributo token del model backend User
-export type UserCreateRequest = Omit<UserResponse, 'token'>;
+export type UserCreateRequest = Omit<User, 'token'>;
