986fc34cac24d0052abc7a5908532d3d07e988c6
jonanv <jonan-vargas23@hotmail.com>
2026-05-21 15:29:00 -0500

    🎨 Add paths with loadChildren to module static-routing and app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'static',
+        loadChildren: () => import('./pages/static/static-module').then(m => m.StaticModule)
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'static/welcome'
+      }
+    ]
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'static/404'
+  }
+];
