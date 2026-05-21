114b2e777be7730f35b54e81f0c07c07cc78c8b8
jonanv <jonan-vargas23@hotmail.com>
2026-05-20 19:03:18 -0500

    ✨ Create component not-found
    ng g component pages/static/pages/not-found --no-standalone

--- frontend/src/app/pages/static/pages/not-found/not-found-module.ts
+++ frontend/src/app/pages/static/pages/not-found/not-found-module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NotFoundRoutingModule } from './not-found-routing-module';
+import { NotFound } from './not-found';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFound],
   imports: [CommonModule, NotFoundRoutingModule],
 })
 export class NotFoundModule {}
