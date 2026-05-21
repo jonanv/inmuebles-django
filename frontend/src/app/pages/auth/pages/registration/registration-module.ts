ff962f25e8a72569828b16b6a0048ed13bd048d7
jonanv <jonan-vargas23@hotmail.com>
2026-05-21 16:15:50 -0500

    ✨ Create a new component registration and import registration-module
    ng g component pages/auth/pages/registration --no-standalone

--- frontend/src/app/pages/auth/pages/registration/registration-module.ts
+++ frontend/src/app/pages/auth/pages/registration/registration-module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Routing
 import { RegistrationRoutingModule } from './registration-routing-module';
 
+// Components
+import { Registration } from './registration';
+
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, RegistrationRoutingModule],
+  declarations: [
+    Registration
+  ],
+  imports: [
+    CommonModule,
+    RegistrationRoutingModule
+  ],
 })
 export class RegistrationModule {}
---
+++ frontend/src/app/pages/auth/pages/registration/registration.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  standalone: false,
+  templateUrl: './registration.html',
+  styleUrl: './registration.scss',
+})
+export class Registration {}
