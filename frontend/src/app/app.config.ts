b06e0da5bf155061f5583df913bd60f98f43a8e6
jonanv <jonan-vargas23@hotmail.com>
2026-05-21 19:08:35 -0500

    ➕ Add dependence ngrx/store and remove providers of firebase
    ng add @ngrx/store@latest

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,21 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
-import { environment } from '../environments/environment.development';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideStorage(() => getStorage()),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
-  ]
+    provideStore(),
+  ],
 };
